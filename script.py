0e7821e355cf8d5632c5e979cd6aecc2025a6198
pabloparejo <parejo@iMac-de-parejo.local>
2015-03-28 20:39:47 +0100

    first working script with fixed file name

--- script.py
+++ script.py
@@ -4,14 +4,41 @@ from os import listdir
 from os.path import isfile, join
 
 
-mod_files = [ f for f in listdir("mod") if isfile(join("mod",f)) ]
-old_files = [ f for f in listdir("old") if isfile(join("old",f)) ]
 
-print mod_files
+files = [ f for f in listdir("old") if isfile(join("old",f)) ]
 
-for mod_file in mod_files:
-	mod_file = open('mod/'+mod_file, 'r')
+"""for file_name in files:
+	with open('mod/'+file_name, 'r') as mod_file:
+		with open('old/'+file_name, 'r') as old_file:
+			with open('new/'+file_name, 'w') as new_file:
+				for mod_line in mod_file:
+					print mod_line.replace("\r\n",'').replace("\t",' ').split(" ")
+					for old_line in old_file:
+						print old_line.replace("\r\n",'').replace("\t",' ').split(" ")
+						break
+						if old_line.split(" ")[0] == mod_line.split(" ")[0]:
+							pass
+					break"""
 
-	for old_file in old_files:
-		new_file = open('new/'+old_file, 'w')
-		old_file = open('mod/'+old_file, 'r')
+file_name = "handling.cfg"
+with open('old/'+file_name, 'r') as old_file:
+	with open('new/'+file_name, 'w') as new_file:
+		with open('mod/'+file_name, 'r') as mod_file:
+			count = 0
+			for old_line in old_file:
+				old_line_clean = old_line.replace("\r\n",'').replace("\t",' ').split(" ")[0]
+				line_to_write = old_line
+
+				for mod_line in mod_file:
+					mod_line_clean = mod_line.replace("\r\n",'').replace("\t",' ').split(" ")[0]
+
+					if old_line_clean == mod_line_clean:
+						line_to_write = mod_line
+						count +=1
+						break
+
+				new_file.write(line_to_write)
+
+				mod_file.seek(0)
+
+			print "The script has been able to recognize %d items to change from %s to %s" %(count, mod_file.name, new_file.name)
